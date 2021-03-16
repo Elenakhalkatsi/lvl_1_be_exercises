7521dcb76ad597b24a934c76bba63655ad65b87a
Elenakhalkatsi <56839808+Elenakhalkatsi@users.noreply.github.com>
2021-03-16 23:33:47 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-let firstVar = 'rame';
-console.log(firstVar);
-let secondVar = 'meore';
-let thirdVar = '';
-console.log(firstVar);
+function calc() {
+    firstNum = Number(prompt("First Number: "));
+    secondNum = Number(prompt("Second Number: "));
+    operation = prompt("Operation: ");
+    let result;
 
+    if (operation === "+") {
+        result = sum(firstNum, secondNum);
+    } else if (operation === "-") {
+        result = sub(firstNum, secondNum);
+    } else if (operation === "*") {
+        result = mult(firstNum, secondNum);
+    } else if (operation === "/") {
+        result = div(firstNum, secondNum);
+    } else {
+        result = "Try again";
+    }
 
-// firtVar = 'rame'
-// thirdVar = ''
-// secondVar = 'meore'
-thirdVar = firstVar;
+    prettyOutput(result);
+}
 
+function sum(num1, num2) {
+    return num1 + num2;
+}
 
-// firtVar = 'meore'
-// thirdVar = 'rame'
-// secondVar = 'meore'
-firstVar = secondVar;
+function sub(num1, num2) {
+    return num1 - num2;
+}
 
-// firtVar = 'meore'
-// thirdVar = 'rame'
-// secondVar = 'meore'
-secondVar = thirdVar;
+function mult(num1, num2) {
+    return num1 * num2;
+}
 
+function dev(num1, num2) {
+    return num1 / num2;
+}
 
-console.log('First: ', firstVar);
-console.log('Second:', secondVar);
-console.log('woohoo');
+function prettyOutput(result) {
+    console.log("Your result: ");
+    console.log("/////// " + result + " ///////");
+}
+
+function anotherOutput(result) {
+    console.log("Your result: ");
+    console.log("******* " + result + " *******");
+}
+
+calc();
